a3909c29940899e5ee6cbb11510eae16d116aaee
Shaunn Barron <shaunnb@gmail.com>
2018-02-01 21:44:30 -0500

    templates wip

--- cmd/vue.go
+++ cmd/vue.go
@@ -2,15 +2,15 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
-
-	"fmt"
+	"text/template"
 
 	"github.com/spf13/cobra"
 )
 
-var app = &BuffaloApp{}
+var vueCmdFlags = &Flags{}
 
 // vueCmd represents the vue command
 var vueCmd = &cobra.Command{
@@ -20,9 +20,9 @@ var vueCmd = &cobra.Command{
 		if len(args) == 0 {
 			return errors.New("you must enter a name for your new application")
 		}
-		app.Name = args[0]
+		vueCmdFlags.Name = args[0]
 
-		flags := buildFlags([]string{"new", app.Name})
+		flags := buildFlags([]string{"new", vueCmdFlags.Name})
 
 		buffalo := exec.Command("buffalo", flags...)
 		buffalo.Stdout = os.Stdout
@@ -35,7 +35,19 @@ var vueCmd = &cobra.Command{
 		}
 
 		fmt.Println("Initial buffalo app has been created. Converting to Vue...")
-		// convert to vue
+		//app := meta.New(fmt.Sprintf("./%s", vueCmdFlags.Name))
+
+		t, err := template.
+			New("templates/package.json.tmpl").
+			ParseFiles("templates/package.json.tmpl")
+		if err != nil {
+			return err
+		}
+
+		err = t.Execute(os.Stdout, nil)
+		if err != nil {
+			return err
+		}
 
 		return nil
 	},
@@ -44,53 +56,53 @@ var vueCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(vueCmd)
 
-	vueCmd.Flags().BoolVarP(&app.Force, "force", "f", false, "delete and remake if the app already exists")
-	vueCmd.Flags().BoolVarP(&app.Verbose, "verbose", "v", false, "verbosely print out the go get commands")
-	vueCmd.Flags().BoolVar(&app.SkipPop, "skip-pop", false, "skips adding pop/soda to your app")
-	vueCmd.Flags().BoolVar(&app.WithDep, "with-dep", false, "adds github.com/golang/dep to your app")
-	vueCmd.Flags().BoolVar(&app.SkipYarn, "skip-yarn", false, "skip to use npm as the asset package manager")
-	vueCmd.Flags().StringVar(&app.DBType, "db-type", "", "specify the type of database you want to use [postgres, mysql, sqlite3]")
-	vueCmd.Flags().StringVar(&app.Docker, "docker", "", "specify the type of Docker file to generate [none, multi, standard]")
-	vueCmd.Flags().StringVar(&app.CIProvider, "ci-provider", "", "specify the type of ci file you would like buffalo to generate [none, travis, gitlab-ci]")
+	vueCmd.Flags().BoolVarP(&vueCmdFlags.Force, "force", "f", false, "delete and remake if the app already exists")
+	vueCmd.Flags().BoolVarP(&vueCmdFlags.Verbose, "verbose", "v", false, "verbosely print out the go get commands")
+	vueCmd.Flags().BoolVar(&vueCmdFlags.SkipPop, "skip-pop", false, "skips adding pop/soda to your app")
+	vueCmd.Flags().BoolVar(&vueCmdFlags.WithDep, "with-dep", false, "adds github.com/golang/dep to your app")
+	vueCmd.Flags().BoolVar(&vueCmdFlags.SkipYarn, "skip-yarn", false, "skip to use npm as the asset package manager")
+	vueCmd.Flags().StringVar(&vueCmdFlags.DBType, "db-type", "", "specify the type of database you want to use [postgres, mysql, sqlite3]")
+	vueCmd.Flags().StringVar(&vueCmdFlags.Docker, "docker", "", "specify the type of Docker file to generate [none, multi, standard]")
+	vueCmd.Flags().StringVar(&vueCmdFlags.CIProvider, "ci-provider", "", "specify the type of ci file you would like buffalo to generate [none, travis, gitlab-ci]")
 }
 
 func buildFlags(flags []string) []string {
-	if app.Force {
+	if vueCmdFlags.Force {
 		flags = append(flags, "--force")
 	}
 
-	if app.Verbose {
+	if vueCmdFlags.Verbose {
 		flags = append(flags, "--verbose")
 	}
 
-	if app.SkipPop {
+	if vueCmdFlags.SkipPop {
 		flags = append(flags, "--skip-pop")
 	}
 
-	if app.WithDep {
+	if vueCmdFlags.WithDep {
 		flags = append(flags, "--with-dep")
 	}
 
-	if app.SkipYarn {
+	if vueCmdFlags.SkipYarn {
 		flags = append(flags, "--skip-yarn")
 	}
 
-	if app.DBType != "" {
-		flags = append(flags, "--db-type", app.DBType)
+	if vueCmdFlags.DBType != "" {
+		flags = append(flags, "--db-type", vueCmdFlags.DBType)
 	}
 
-	if app.Docker != "" {
-		flags = append(flags, "--docker", app.Docker)
+	if vueCmdFlags.Docker != "" {
+		flags = append(flags, "--docker", vueCmdFlags.Docker)
 	}
 
-	if app.CIProvider != "" {
-		flags = append(flags, "--ci-provider", app.CIProvider)
+	if vueCmdFlags.CIProvider != "" {
+		flags = append(flags, "--ci-provider", vueCmdFlags.CIProvider)
 	}
 
 	return flags
 }
 
-type BuffaloApp struct {
+type Flags struct {
 	Name       string
 	Force      bool
 	Verbose    bool
